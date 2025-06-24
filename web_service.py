5b9421fa55e5a0706a54261c27d23a7a3f9d7671
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2025-06-24 16:10:59 +0300

    handler for root route

--- web_service.py
+++ web_service.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return "<p>Please press <a href=poks>here</a>."
+
 @app.route("/poks")
 def poks():
     return "<h1>Poks</h1><p>This is the content of the address /poks.</p>"
